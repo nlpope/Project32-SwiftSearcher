4dcd21afd452295aa6b05a09f5c9a01cd75698c8
Noah Pope <npope@tutanota.com>
2025-06-03 18:55:36 -0500

    Initial Commit

---
+++ Project32-SwiftSearcher/ViewController.swift
@@ -0,0 +1,16 @@
+//  File: ViewController.swift
+//  Project: Project32-SwiftSearcher
+//  Created by: Noah Pope on 6/3/25.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Project32-SwiftSearcherTests/Project32_SwiftSearcherTests.swift
@@ -0,0 +1,14 @@
+//  File: Project32_SwiftSearcherTests.swift
+//  Project: Project32-SwiftSearcherTests
+//  Created by: Noah Pope on 6/3/25.
+
+import Testing
+@testable import Project32_SwiftSearcher
+
+struct Project32_SwiftSearcherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
