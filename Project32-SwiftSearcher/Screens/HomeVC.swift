143a56900245979d98778c96d387c12568795680
Noah Pope <npope@tutanota.com>
2025-06-03 19:06:55 -0500

    initial commit

---
+++ Project32-SwiftSearcher/Screens/HomeVC.swift
@@ -0,0 +1,13 @@
+//  File: HomeVC.swift
+//  Project: Project32-SwiftSearcher
+//  Created by: Noah Pope on 6/3/25.
+
+import UIKit
+
+class HomeVC: UITableViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
